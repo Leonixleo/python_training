cc29e9842e3af622241456165b9d88517947ba68
leo <leo.ovakimidis@gmail.com>
2023-04-17 13:21:49 +0300

    "Фикстура": общий код инициализации и завершения ДЗ 1

--- test_add_contacts.py
+++ test_add_contacts.py
@@ -1,111 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import unittest
+import pytest
 from contact import Contact
+from application_for_contacts import Application_for_contact
 
 
-class ContacttestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def filling_contact_fields_test_case(self):
-        wd = self.wd
-        self.open_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_contact_form(wd)
-        self.filling_in_the_fields(wd, Contact(firstname="levan", middlename="levanovich", lastname ="ovakimidis", nickname="levanich",
-                                                company="levancompany", address="Moscow ave.", home_num="9884737",
-                                                mob_num="+3993774", work_num="44444", email1="levan@gms.tech", bday="18",
-                                                bmanth="December", byear="1998", amonth="November",
-                                                aday="17", ayear="2000"))
-        self.return_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        # Logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_home_page(self, wd):
-        # Return home page
-        wd.find_element_by_link_text("home page").click()
-
-    def filling_in_the_fields(self, wd, contacts):
-        # Filling in the fields and create
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contacts.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contacts.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contacts.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contacts.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contacts.home_num)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contacts.mob_num)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contacts.work_num)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contacts.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contacts.bday)
-        wd.find_element_by_xpath("//option[@value='18']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contacts.bmanth)
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contacts.byear)
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contacts.amonth)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[12]").click()
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contacts.aday)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[19]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contacts.ayear)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def open_contact_form(self, wd):
-        # Open contact forms and create
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        # Login
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-
-    def open_page(self, wd):
-        # Open page
-        wd.get("http://localhost/addressbook/index.php")
+@pytest.fixture
+def app(request):
+    fixture = Application_for_contact
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def tearDown(self):
-        self.wd.quit()
 
-
-if __name__ == "__main__":
-    unittest.main()
+def filling_contact_fields_test_case(app):
+        app.login(username="admin", password="secret")
+        app.open_contact_form()
+        app.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
+                                               nickname="levanich",
+                                               company="levancompany", address="Moscow ave.", home_num="9884737",
+                                               mob_num="+3993774", work_num="44444", email1="levan@gms.tech", bday="18",
+                                               bmanth="December", byear="1998", amonth="November",
+                                               aday="17", ayear="2000"))
+        app.return_home_page()
+        app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -3,6 +3,7 @@ import pytest
 from group import Group
 from application import Application
 
+
 @pytest.fixture()
 def app(request):
     fixture = Application()
@@ -11,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-        app.login(username="admin", password= "secret")
-        app.create_group(Group(name="asdfghjkl", header="aswqedfghjkl", footer="sdfghjkl;"))
-        app.logout()
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="asdfghjkl", header="aswqedfghjkl", footer="sdfghjkl;"))
+    app.logout()
 
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
-        app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
