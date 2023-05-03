796851dd59aec1f3210b138a577181707fdaf678
leo <leo.ovakimidis@gmail.com>
2023-05-04 00:18:19 +0300

    Выделен класс  ContactsHelper

--- Test/test_add_contacts.py
+++ Test/test_add_contacts.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_filling_contact_fields(app):
         app.session.login(username="admin", password="secret")
-        app.open_contact_form()
-        app.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
+        app.contacts.open_form()
+        app.contacts.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
                                                nickname="levanich",
                                                company="levancompany", address="Moscow ave.", home_num="9884737",
                                                mob_num="+3993774", work_num="44444", email1="levan@gms.tech", bday="18",
                                                bmanth="December", byear="1998", amonth="November",
                                                aday="17", ayear="2000"))
-        app.return_home_page()
+        app.contacts.return_home_page()
         app.session.logout()
