4be764d1f45c42625d68dd76053a25a05aebcaa6
leo <leo.ovakimidis@gmail.com>
2023-05-04 00:57:33 +0300

    Тест на удоление группы

--- Test/test_add_contacts.py
+++ Test/test_add_contacts.py
@@ -1,24 +1,16 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_filling_contact_fields(app):
         app.session.login(username="admin", password="secret")
         app.contacts.open_form()
         app.contacts.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
-                                               nickname="levanich",
+                                                nickname="levanich",
                                                company="levancompany", address="Moscow ave.", home_num="9884737",
                                                mob_num="+3993774", work_num="44444", email1="levan@gms.tech", bday="18",
                                                bmanth="December", byear="1998", amonth="November",
                                                aday="17", ayear="2000"))
+
         app.contacts.return_home_page()
         app.session.logout()
