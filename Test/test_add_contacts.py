9240228aae2aceb176a68f7eefae3fa833603e84
leo <leo.ovakimidis@gmail.com>
2023-05-10 14:28:30 +0300

    Исправление contacts на сontact

--- Test/test_add_contacts.py
+++ Test/test_add_contacts.py
@@ -4,13 +4,13 @@ from model.contact import Contact
 
 def test_filling_contact_fields(app):
         app.session.login(username="admin", password="secret")
-        app.contacts.open_form()
-        app.contacts.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
+        app.contact.open_form()
+        app.contact.filling_in_the_fields(Contact(firstname="levan", middlename="levanovich", lastname="ovakimidis",
                                                nickname="levanich",
                                               company="levancompany", address="Moscow ave.", home_num="9884737",
                                               mob_num="+3993774", work_num="44444", email1="levan@gms.tech", bday="18",
                                               bmanth="December", byear="1998", amonth="November",
                                                aday="17", ayear="2000"))
 
-        app.contacts.return_home_page()
+        app.contact.return_home_page()
         app.session.logout()
