3f7b11db3b2c7891534fb7829ba3903a12a347ed
leo <leo.ovakimidis@gmail.com>
2023-06-28 10:31:03 +0300

    Задание №11:

--- fixture/group.py
+++ fixture/group.py
@@ -1,6 +1,10 @@
+from selenium.webdriver.common.by import By
+from model.group import Group
+
+
 class GroupHelper:
 
-    def __init__(self,app):
+    def __init__(self, app):
         self.app = app
 
     def return_to_group_page(self):
@@ -75,7 +79,7 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        if not wd.current_url.endswith("/index.php") and len(wd.find_elements_by_name("new")) > 0:
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
             wd.find_element_by_link_text("groups").click()
 
     def count(self):
@@ -83,3 +87,12 @@ class GroupHelper:
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+            return groups
